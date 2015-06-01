2a0657a176455144f3a5feeddc20d5b88139a5df
Mathieu Comandon <strycore@gmail.com>
2015-05-31 23:19:58 -0700

    Actually record the relative path

--- fili/commands.py
+++ fili/commands.py
@@ -17,7 +17,7 @@ def index_list():
 
 
 def index_file(path, fastsum, sha1, fileinfo, scan):
-    path = fileinfo['path'].encode('utf-8', 'surrogateescape')
+    path = path.encode('utf-8', 'surrogateescape')
     sys.stdout.write("Indexing {}\n".format(path))
     indexed_file = File(
         path=path,
